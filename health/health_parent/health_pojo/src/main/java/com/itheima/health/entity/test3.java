7c29cfe9b51daf35d195fd893613216282a0503c
Kay <Kay.com>
2019-09-30 19:56:54 +0800

    health创建1909301756

--- health/health_parent/health_pojo/src/main/java/com/itheima/health/entity/test3.java
+++ health/health_parent/health_pojo/src/main/java/com/itheima/health/entity/test3.java
@@ -1,7 +1,12 @@
 package com.itheima.health.entity;
 
 public class test3 {
-    private String zhangsan;
+
+   private String zhangsan;
    private int age;
    private String password;
+   private String password123;
+   private String password456;
+   private String password789;
+   private String name;
 }
